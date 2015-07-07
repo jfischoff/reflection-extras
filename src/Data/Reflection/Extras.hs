fffab58e6bba030d9eedb0589eb8317cb3ac4a36
Renzo Carbonara <gnuk0001@gmail.com>
2015-07-06 21:58:21 -0300

    Bump dependencies

--- src/Data/Reflection/Extras.hs
+++ src/Data/Reflection/Extras.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE KindSignatures#-} 
+{-# LANGUAGE KindSignatures#-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -14,7 +14,7 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE CPP #-}
-module Data.Reflection.Extras 
+module Data.Reflection.Extras
    ( using
    , usingT
    , reifyInstance
@@ -25,7 +25,7 @@ module Data.Reflection.Extras
    , Def (..)
    , FromJSON (..)
    , ToJSON (..)
-   ) where 
+   ) where
 import Data.Constraint
 import Data.Constraint.Unsafe
 import Data.Monoid
@@ -43,18 +43,20 @@ import Control.Applicative
 -- I made this a functor to make the instances easier
 newtype Lift (p :: * -> Constraint) (s :: *) (a :: *) = Lift { lower :: a }
    deriving (Functor)
-   
+
 instance Applicative (Lift p s) where
    pure              = Lift
    Lift f <*> Lift x = Lift $ f x
 
-makeIso ''Lift
+lift :: Iso' a (Lift p s a)
+lift = iso Lift lower
 
 newtype ProxyLift (p :: * -> Constraint) (a :: *) (s :: *) = PLift { plower :: a }
 
-makeIso ''ProxyLift 
+pLift :: Iso' a (ProxyLift p a s)
+pLift = iso PLift plower
 
-flipS :: Iso' (Lift p s a) (ProxyLift p a s) 
+flipS :: Iso' (Lift p s a) (ProxyLift p a s)
 flipS = from lift . pLift
 
 class ReifiableConstraint p where
@@ -62,7 +64,7 @@ class ReifiableConstraint p where
   reifiedIns :: forall s a. Reifies s (Def p a) :- p (Lift p s a)
 --  default reifiedIns :: forall s a. p (Lift p s a)
 --                     => Reifies s (Def p a) :- p (Lift p s a)
---  reifiedIns = Sub (Dict :: Reifies s (Def p a) 
+--  reifiedIns = Sub (Dict :: Reifies s (Def p a)
 --                         => Dict (p (Lift p s a)))
 
 --------------------------------------------------------------------------------
@@ -79,10 +81,10 @@ asProxyOf :: f s -> Proxy s -> f s
 asProxyOf a _ = a
 
 -- | Choose a dictionary for a local type class instance.
---   
+--
 --   >>> using (Monoid (+) 0) $ mempty <> 10 <> 12
 --   > 12
---   
+--
 using :: forall p a. ReifiableConstraint p => Def p a -> (p a => a) -> a
 using d m = reify d $ \(_ :: Proxy s) ->
   let replaceProof :: Reifies s (Def p a) :- p a
@@ -128,7 +130,7 @@ instance Reifies s (Def Enum a) => Enum (Lift Enum s a) where
    enumFromThenTo a b c = map Lift $ enumFromThenTo_ REFLECT (lower a) (lower b) (lower c)
 
 instance ReifiableConstraint Enum where
-  data Def Enum a = Enum 
+  data Def Enum a = Enum
       { succ_           :: a -> a
       , pred_           :: a -> a
       , toEnum_         :: Int -> a
@@ -145,7 +147,7 @@ instance Reifies s (Def Bounded a) => Bounded (Lift Bounded s a) where
   maxBound = Lift $ maxBound_ REFLECT
 
 instance ReifiableConstraint Bounded where
-  data Def Bounded a = Bounded 
+  data Def Bounded a = Bounded
      { minBound_ :: a
      , maxBound_ :: a
      }
@@ -155,20 +157,20 @@ instance Reifies s (Def Num a) => Num (Lift Num s a) where
   (+)         = liftA2 ((+.) REFLECT)
   (*)         = liftA2 ((*.) REFLECT)
   (-)         = liftA2 ((-.) REFLECT)
-  negate      = fmap (negate_ REFLECT) 
-  abs         = fmap (abs_ REFLECT) 
-  signum      = fmap (signum_ REFLECT) 
+  negate      = fmap (negate_ REFLECT)
+  abs         = fmap (abs_ REFLECT)
+  signum      = fmap (signum_ REFLECT)
   fromInteger = Lift . fromInteger_  REFLECT
 
 instance ReifiableConstraint Num where
-  data Def Num a = Num 
+  data Def Num a = Num
      { (+.)         :: a -> a -> a
      , (*.)         :: a -> a -> a
      , (-.)         :: a -> a -> a
      , negate_      :: a -> a
-     , abs_         :: a -> a 
+     , abs_         :: a -> a
      , signum_      :: a -> a
-     , fromInteger_ :: Integer -> a  
+     , fromInteger_ :: Integer -> a
      }
   reifiedIns = Sub Dict
 
@@ -177,24 +179,24 @@ instance (Reifies s (Def Real a)) => Eq (Lift Real s a) where
 
 instance (Reifies s (Def Real a)) => Ord (Lift Real s a) where
    compare a b = (compare_ . ordDef) REFLECT (lower a) (lower b)
-      
+
 instance (Reifies s (Def Real a)) => Num (Lift Real s a) where
    (+)         = liftA2 ((+.) $ numDef REFLECT)
    (*)         = liftA2 ((*.) $ numDef REFLECT)
    (-)         = liftA2 ((-.) $ numDef REFLECT)
-   negate      = fmap (negate_ $ numDef REFLECT) 
-   abs         = fmap (abs_ $ numDef REFLECT) 
-   signum      = fmap (signum_ $ numDef REFLECT) 
+   negate      = fmap (negate_ $ numDef REFLECT)
+   abs         = fmap (abs_ $ numDef REFLECT)
+   signum      = fmap (signum_ $ numDef REFLECT)
    fromInteger = Lift . (fromInteger_ . numDef) REFLECT
 
 instance Reifies s (Def Real a) => Real (Lift Real s a) where
   toRational a = toRational_ REFLECT (lower a)
 
 instance ReifiableConstraint Real where
- data Def Real a = Real 
+ data Def Real a = Real
    { toRational_ :: a -> Rational
    , ordDef      :: Def Ord a
-   , numDef      :: Def Num a 
+   , numDef      :: Def Num a
    }
  reifiedIns = Sub Dict
 
@@ -210,9 +212,9 @@ instance Reifies s (Def Integral a) => Integral (Lift Integral s a) where
    quotRem a b = over both Lift $ quotRem_ REFLECT (lower a) (lower b)
    divMod  a b = over both Lift $ divMod_  REFLECT (lower a) (lower b)
    toInteger a = toInteger_ REFLECT (lower a)
-   
+
 instance ReifiableConstraint Integral where
-  data Def Integral a = Integral 
+  data Def Integral a = Integral
      { quot_      :: a -> a -> a
      , rem_       :: a -> a -> a
      , div_       :: a -> a -> a
@@ -230,7 +232,7 @@ instance Reifies s (Def Fractional a) => Fractional (Lift Fractional s a) where
   fromRational a b = Lift $ fromRational_ REFLECT (lower a) (lower b)
 
 instance ReifiableConstraint Fractional where
-  data Def Fractional a = Fractional 
+  data Def Fractional a = Fractional
       { (/.)          :: a -> a -> a
       , recip_        :: a -> a
       , fromRational_ :: Rational -> a
@@ -239,27 +241,27 @@ instance ReifiableConstraint Fractional where
 
 instance Reifies s (Def Floating a) => Floating (Lift Floating s a) where
    pi          = Lift $ pi_      reflect (Proxy :: Proxy s)
-   exp     a   = Lift $ exp_     REFLECT (lower a) 
-   sqrt    a   = Lift $ sqrt_    REFLECT (lower a) 
-   log     a   = Lift $ log_     REFLECT (lower a) 
+   exp     a   = Lift $ exp_     REFLECT (lower a)
+   sqrt    a   = Lift $ sqrt_    REFLECT (lower a)
+   log     a   = Lift $ log_     REFLECT (lower a)
    (**)    a b = Lift $ (**.)    REFLECT (lower a) (lower b)
    logBase a b = Lift $ logBase_ REFLECT (lower a) (lower b)
-   sin     a   = Lift $ sin_     REFLECT (lower a) 
-   tan     a   = Lift $ tan_     REFLECT (lower a) 
-   cos     a   = Lift $ cos_     REFLECT (lower a)  
-   asin    a   = Lift $ asin_    REFLECT (lower a) 
-   atan    a   = Lift $ atan_    REFLECT (lower a) 
-   acos    a   = Lift $ acos_    REFLECT (lower a) 
-   sinh    a   = Lift $ sinh_    REFLECT (lower a) 
-   tanh    a   = Lift $ tanh_    REFLECT (lower a) 
-   cosh    a   = Lift $ cosh_    REFLECT (lower a) 
-   asinh   a   = Lift $ asinh_   REFLECT (lower a)  
-   atanh   a   = Lift $ atanh_   REFLECT (lower a)  
-   acosh   a   = Lift $ acosh_   REFLECT (lower a) 
+   sin     a   = Lift $ sin_     REFLECT (lower a)
+   tan     a   = Lift $ tan_     REFLECT (lower a)
+   cos     a   = Lift $ cos_     REFLECT (lower a)
+   asin    a   = Lift $ asin_    REFLECT (lower a)
+   atan    a   = Lift $ atan_    REFLECT (lower a)
+   acos    a   = Lift $ acos_    REFLECT (lower a)
+   sinh    a   = Lift $ sinh_    REFLECT (lower a)
+   tanh    a   = Lift $ tanh_    REFLECT (lower a)
+   cosh    a   = Lift $ cosh_    REFLECT (lower a)
+   asinh   a   = Lift $ asinh_   REFLECT (lower a)
+   atanh   a   = Lift $ atanh_   REFLECT (lower a)
+   acosh   a   = Lift $ acosh_   REFLECT (lower a)
 
 instance ReifiableConstraint Floating where
-  data Def Floating a = Floating 
-      { pi_      :: a 
+  data Def Floating a = Floating
+      { pi_      :: a
       , exp_     :: a -> a
       , sqrt_    :: a -> a
       , log_     :: a -> a
@@ -267,15 +269,15 @@ instance ReifiableConstraint Floating where
       , logBase_ :: a -> a -> a
       , sin_     :: a -> a
       , tan_     :: a -> a
-      , cos_     :: a -> a 
+      , cos_     :: a -> a
       , asin_    :: a -> a
       , atan_    :: a -> a
       , acos_    :: a -> a
       , sinh_    :: a -> a
       , tanh_    :: a -> a
       , cosh_    :: a -> a
-      , asinh_   :: a -> a 
-      , atanh_   :: a -> a 
+      , asinh_   :: a -> a
+      , atanh_   :: a -> a
       , acosh_   :: a -> a
       }
   reifiedIns = Sub Dict
@@ -286,10 +288,10 @@ instance Reifies s (Def RealFrac a) => RealFrac (Lift RealFrac s a) where
    round          a =      Lift $ round_          REFLECT (lower a)
    ceiling        a =      Lift $ ceiling_        REFLECT (lower a)
    floor          a =      Lift $ floor_          REFLECT (lower a)
-   
+
 
 instance ReifiableConstraint RealFrac where
-  data Def RealFrac a = RealFrac 
+  data Def RealFrac a = RealFrac
       { properFraction_ :: Integral b => a -> (b, a)
       , truncate_       :: Integral b => a -> b
       , round_          :: Integral b => a -> b
@@ -299,22 +301,22 @@ instance ReifiableConstraint RealFrac where
   reifiedIns = Sub Dict
 
 instance Reifies s (Def RealFloat a) => RealFloat (Lift RealFloat s a) where
-   floatRadix     a   = floatRadix_     REFLECT (lower a) 
-   floatDigits    a   = floatDigits_    REFLECT (lower a) 
-   floatRange     a   = floatRange_     REFLECT (lower a) 
-   decodeFloat    a   = decodeFloat_    REFLECT (lower a) 
+   floatRadix     a   = floatRadix_     REFLECT (lower a)
+   floatDigits    a   = floatDigits_    REFLECT (lower a)
+   floatRange     a   = floatRange_     REFLECT (lower a)
+   decodeFloat    a   = decodeFloat_    REFLECT (lower a)
    encodeFloat    a b = encodeFloat_    (reflect b) (lower a) (lower b)
-   exponent       a   = exponent_       REFLECT (lower a) 
+   exponent       a   = exponent_       REFLECT (lower a)
    significand    a b = significand_    (reflect b) (lower a) (lower b)
    scaleFloat     a b = scaleFloat_     (reflect b) (lower a) (lower b)
-   isInfinite     a   = isInfinite_     REFLECT (lower a) 
-   isDenormalized a   = isDenormalized_ REFLECT (lower a) 
-   isNegativeZero a   = isNegativeZero_ REFLECT (lower a) 
-   isIEEE         a   = isIEEE_         REFLECT (lower a) 
-   atan2          a   = atan2_          REFLECT (lower a) 
+   isInfinite     a   = isInfinite_     REFLECT (lower a)
+   isDenormalized a   = isDenormalized_ REFLECT (lower a)
+   isNegativeZero a   = isNegativeZero_ REFLECT (lower a)
+   isIEEE         a   = isIEEE_         REFLECT (lower a)
+   atan2          a   = atan2_          REFLECT (lower a)
 
 instance ReifiableConstraint RealFloat where
-  data Def RealFloat a = RealFloat 
+  data Def RealFloat a = RealFloat
       { floatRadix_  :: a -> Integer
       , floatDigits_ :: a -> Int
       , floatRange_  :: a -> (Int, Int)
@@ -335,13 +337,13 @@ instance ReifiableConstraint RealFloat where
 {-
 I think this will need a reifyable constraint1
 instance Reifies s (Def Monad a) => Monad (Lift Monad s a) where
-   (>>=)  = 
-   (>>)   = 
-   return = 
-   fail   = 
+   (>>=)  =
+   (>>)   =
+   return =
+   fail   =
 
 instance ReifiableConstraint Monad where
-  data Def Monad a = Monad 
+  data Def Monad a = Monad
       { (>>=.) :: forall a b. m a -> (a -> m b) -> m b
       , (>>.)  :: forall a b. m a -> m b -> m b
       , return :: a -> m a
@@ -363,7 +365,7 @@ instance ReifiableConstraint Read where
 
 instance Reifies s (Def Read a) => Read (Lift Read s a) where
   readsPrec x s = over _1 Lift <$> readsPrec_ REFLECT x s
-  readList  s   = over _1 (fmap Lift) 
+  readList  s   = over _1 (fmap Lift)
                <$> readList_  REFLECT s
 
 instance ReifiableConstraint Eq where
@@ -371,7 +373,7 @@ instance ReifiableConstraint Eq where
   reifiedIns = Sub Dict
 
 instance Reifies s (Def Eq a) => Eq (Lift Eq s a) where
-   x == y = eq_ REFLECT (lower x) (lower y) 
+   x == y = eq_ REFLECT (lower x) (lower y)
 
 instance ReifiableConstraint Ord where
   data Def Ord a = Ord { compare_ :: a -> a -> Ordering }
@@ -388,14 +390,14 @@ instance ReifiableConstraint Monoid where
   reifiedIns = Sub Dict
 
 instance Reifies s (Def Monoid a) => Monoid (Lift Monoid s a) where
-  mappend = liftA2 (mappend_ REFLECT) 
+  mappend = liftA2 (mappend_ REFLECT)
   mempty  = pure $ mempty_ REFLECT
 
 -- Aeson Instances
 instance ReifiableConstraint FromJSON where
    data Def FromJSON a = FromJSON { parseJSON_ :: Value -> Parser a }
    reifiedIns = Sub Dict
-   
+
 instance Reifies s (Def FromJSON a) => FromJSON (Lift FromJSON s a) where
    parseJSON = fmap pure . parseJSON_ REFLECT
 
